dbe5884fc676f7d6e0418d3778c272b3104f94a4
inventariosarda <giulianova2025@gmail.com>
2026-02-08 00:11:43 +0100

    Add files via upload

---
+++ js/firebase-config-global.js
@@ -0,0 +1,9 @@
+// Firebase configuration (global for compat SDK)
+window.firebaseConfig = {
+  apiKey: "AIzaSyDkljDA6GmI_1PFssiSxWtcmmkigu7Csk4",
+  authDomain: "programma-magazzino.firebaseapp.com",
+  projectId: "programma-magazzino",
+  storageBucket: "programma-magazzino.firebasestorage.app",
+  messagingSenderId: "275162226627",
+  appId: "1:275162226627:web:83ea00dc122517623fa776"
+};
